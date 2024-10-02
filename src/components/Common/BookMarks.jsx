06109d2e6f48b4f130577588c1eb6b463f3d26ec
thanhtuvietnam <luuthanhtung555@gmail.com>
2024-10-02 12:32:19 +0700

    none

--- src/components/Common/BookMarks.jsx
+++ src/components/Common/BookMarks.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
-import { useAppdispatch, useAppSelector } from '../../store/hook';
-import SectionTitle from './SectionTitle';
-import { RightBarCar } from '../MainLayOut';
-import { IMG_URL } from '../../shared/constant';
-import { Link } from 'react-router-dom';
-import { removeAllBookmarks, removeBookmarks } from '../../store/bookmarks/bookmarks.slice';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState, useMemo, useCallback } from "react";
+import { useAppdispatch, useAppSelector } from "../../store/hook";
+import SectionTitle from "./SectionTitle";
+import { RightBarCar } from "../MainLayOut";
+import { IMG_URL } from "../../shared/constant";
+import { Link } from "react-router-dom";
+import {
+  removeAllBookmarks,
+  removeBookmarks,
+} from "../../store/bookmarks/bookmarks.slice";
+import "react-toastify/dist/ReactToastify.css";
 
 const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
   const [isDeleting, setIsDeleting] = useState(false); // State để theo dõi trạng thái xóa
   const dispatch = useAppdispatch();
   const bookmarks = useAppSelector((state) => state.bookmarks.bookmarks);
-  const { loading, error, success, userInfo } = useAppSelector((state) => state.auth);
+  const { loading, error, success, userInfo } = useAppSelector(
+    (state) => state.auth,
+  );
   // console.log(bookmarks);
 
   const handleRemoveBookMark = useCallback(
@@ -20,7 +25,7 @@ const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
         dispatch(removeBookmarks(bookmarkId));
       }
     },
-    [bookmarks.length, dispatch]
+    [bookmarks.length, dispatch],
   );
 
   const handleDeleteAll = useCallback(
@@ -33,15 +38,15 @@ const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
         });
       }
     },
-    [bookmarks.length, dispatch, userInfo?.uid]
+    [bookmarks.length, dispatch, userInfo?.uid],
   );
 
   const renderTitle = useMemo(() => {
     return (
       <SectionTitle
         disable={true}
-        hidden={'hidden'}
-        sectionFilm='BookMarks'
+        hidden={"hidden"}
+        sectionFilm="BookMarks"
         showDeleteAll={true}
         showSeeAll={false}
         onDeleteAll={handleDeleteAll}
@@ -54,9 +59,7 @@ const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
     return (
       bookmarks &&
       bookmarks.map((bookmark) => (
-        <Link
-          key={bookmark?.id}
-          to={`/chitiet-phim/${bookmark.slug}`}>
+        <Link key={bookmark?.id} to={`/chitiet-phim/${bookmark.slug}`}>
           <RightBarCar
             setShowDropdown={toggleDropdown}
             movieName={bookmark?.movieName}
@@ -73,10 +76,10 @@ const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
     <>
       {isBookmarkDropdownOpen && (
         <div
-          className='scroll-bar-custom absolute bg-[#222222] w-[300px] sm:w-[400px] top-0 right-0 translate-y-[46px]  rounded-md shadow-lg z-50 max-h-[300px] sm:max-h-[400px] md:max-h-[470px] lg:max-h-[550px] xl:max-h-[650px] overflow-y-scroll  border-[1px] border-[#684808]'
+          className="scroll-bar-custom absolute bg-[#222222] w-[300px] sm:w-[400px] top-0 right-0 translate-y-[46px]  rounded-md shadow-lg z-50 max-h-[300px] sm:max-h-[400px] md:max-h-[470px] lg:max-h-[550px] xl:max-h-[650px] overflow-y-scroll  border-[1px] border-[#684808]"
           // ref={dropdownRef}
         >
-          <div className='p-2'>{renderTitle}</div>
+          <div className="p-2">{renderTitle}</div>
           <ul>{renderbookMarkResults}</ul>
         </div>
       )}
@@ -84,6 +87,6 @@ const BookMarks = React.memo(({ isBookmarkDropdownOpen, toggleDropdown }) => {
   );
 });
 
-BookMarks.displayName = 'BookMarks';
+BookMarks.displayName = "BookMarks";
 
 export default BookMarks;
--- src/components/Common/MovieCategory.jsx
+++ src/components/Common/MovieCategory.jsx
@@ -1,163 +1,186 @@
-import React, { useEffect, useMemo } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { NoteViewer, CardItem, Filter, PaginationCom, SectionTitle, TrendingNow, BreadCrumb } from './index.js';
-import { IMG_URL, noteLine, titlePhimBo } from '../../shared/constant.js';
-import { classifyAddon } from '../../shared/utils.js';
-import PropTypes from 'prop-types';
-import SkeletonForAll from '../Skeleton/SkeletonForAll/SkeletonForAll.jsx';
-import { useGetMoviesByCategoryQuery } from '../../store/apiSlice/homeApi.slice.js';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useAppSelector } from '../../store/hook.js';
-import { setCurrentPage } from '../../store/searchSlice/searchSlice.js';
-import { icons } from '../../shared/icon.js';
-import { Helmet } from 'react-helmet';
+import PropTypes from "prop-types";
+import React, { useEffect, useMemo } from "react";
+import { Helmet } from "react-helmet";
+import { Link, useLocation } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { IMG_URL, noteLine } from "../../shared/constant.js";
+import { icons } from "../../shared/icon.js";
+import { classifyAddon } from "../../shared/utils.js";
+import { useGetMoviesByCategoryQuery } from "../../store/apiSlice/homeApi.slice.js";
+import { useAppSelector } from "../../store/hook.js";
+import { setCurrentPage } from "../../store/searchSlice/searchSlice.js";
+import SkeletonForAll from "../Skeleton/SkeletonForAll/SkeletonForAll.jsx";
+import {
+  BreadCrumb,
+  CardItem,
+  Filter,
+  NoteViewer,
+  PaginationCom,
+  SectionTitle,
+  TrendingNow,
+} from "./index.js";
 
 const { IoWarningSharp } = icons;
 
-const MovieCategory = React.memo(({ sectionTitle, dataResults, totalItemsSearch, categorySlug, categoryBreadCrumb, OthersBreadCrumb, hiddenOther, movieSortValue, numberSlice, title, metaDescription }) => {
-  const selectedSlug = useAppSelector((state) => state.submenu.slug);
-  const selectedType = useAppSelector((state) => state.submenu.type);
-  const location = useLocation();
+const MovieCategory = React.memo(
+  ({
+    sectionTitle,
+    dataResults,
+    totalItemsSearch,
+    categorySlug,
+    categoryBreadCrumb,
+    OthersBreadCrumb,
+    hiddenOther,
+    movieSortValue,
+    numberSlice,
+    title,
+    metaDescription,
+  }) => {
+    const selectedSlug = useAppSelector((state) => state.submenu.slug);
+    const selectedType = useAppSelector((state) => state.submenu.type);
+    const location = useLocation();
 
-  const pageType = location.pathname === '/tim-kiem' ? 'search' : 'normal';
+    const pageType = location.pathname === "/tim-kiem" ? "search" : "normal";
 
-  const currentPage = useAppSelector((state) => state.search.currentPage);
+    const currentPage = useAppSelector((state) => state.search.currentPage);
 
-  const { data: categoryData, isLoading, isFetching, isError, error } = useGetMoviesByCategoryQuery({ category: categorySlug, page: currentPage }, { skip: !categorySlug });
+    const {
+      data: categoryData,
+      isLoading,
+      isFetching,
+      isError,
+      error,
+    } = useGetMoviesByCategoryQuery(
+      { category: categorySlug, page: currentPage },
+      { skip: !categorySlug },
+    );
 
-  const limit = 24;
+    const limit = 24;
 
-  const totalPages = useMemo(() => {
-    return totalItemsSearch ? Math.ceil(totalItemsSearch / limit) : Math.ceil((categoryData?.data?.params?.pagination?.totalItems || 0) / limit);
-  }, [totalItemsSearch, categoryData, limit]);
+    const totalPages = useMemo(() => {
+      return totalItemsSearch
+        ? Math.ceil(totalItemsSearch / limit)
+        : Math.ceil(
+            (categoryData?.data?.params?.pagination?.totalItems || 0) / limit,
+          );
+    }, [totalItemsSearch, categoryData, limit]);
 
-  const searchPageError = useAppSelector((state) => state.loadingState.Error);
+    const searchPageError = useAppSelector((state) => state.loadingState.Error);
 
-  useEffect(() => {
-    if (isError && error) {
-      toast('BẠN VUI LÒNG BẤM F5 HOẶC BẤM TẢI LẠI TRANG', {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'light',
-      });
-    } else if (searchPageError) {
-      toast('BẠN THỬ THỰC HIỆN LẠI THAO TÁC VÀ F5 XEM', {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'light',
-      });
-    }
-  }, [isError, error, searchPageError]);
+    useEffect(() => {
+      if (isError && error) {
+        toast("BẠN VUI LÒNG BẤM F5 HOẶC BẤM TẢI LẠI TRANG", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else if (searchPageError) {
+        toast("BẠN THỬ THỰC HIỆN LẠI THAO TÁC VÀ F5 XEM", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    }, [isError, error, searchPageError]);
 
-  const renderMovieItems = useMemo(() => {
-    const items = dataResults || categoryData?.data?.items;
-    if (!items || items.length === 0) {
-      return (
-        <div className='col-span-full text-center text-white text-lg'>
-          <div className='flex gap-2 items-center justify-center'>
-            <IoWarningSharp
-              color='yellow'
-              size={45}
-            />
-            <h1>Rất tiếc, chúng tôi không có phim cho mục này...</h1>
+    const renderMovieItems = useMemo(() => {
+      const items = dataResults || categoryData?.data?.items;
+      if (!items || items.length === 0) {
+        return (
+          <div className="col-span-full text-center text-white text-lg">
+            <div className="flex gap-2 items-center justify-center">
+              <IoWarningSharp color="yellow" size={45} />
+              <h1>Rất tiếc, chúng tôi không có phim cho mục này...</h1>
+            </div>
           </div>
-        </div>
+        );
+      }
+      return items?.map((item) => (
+        <Link to={`/chitiet-phim/${item.slug}`} key={item._id}>
+          <CardItem
+            image={`${IMG_URL}/${item?.thumb_url}`}
+            title={item?.name}
+            originalName={item?.origin_name}
+            quality={item?.quality}
+            lang={item?.lang}
+            addOn={classifyAddon(item)}
+            cardItemQualang="cardItemQualang"
+          />
+        </Link>
+      ));
+    }, [dataResults, categoryData]);
+
+    if (isFetching) {
+      return (
+        <SkeletonForAll withSlider={false} cardCount={24} sectionCount={1} />
       );
     }
-    return items?.map((item) => (
-      <Link
-        to={`/chitiet-phim/${item.slug}`}
-        key={item._id}>
-        <CardItem
-          image={`${IMG_URL}/${item?.thumb_url}`}
-          title={item?.name}
-          originalName={item?.origin_name}
-          quality={item?.quality}
-          lang={item?.lang}
-          addOn={classifyAddon(item)}
-          cardItemQualang='cardItemQualang'
-        />
-      </Link>
-    ));
-  }, [dataResults, categoryData]);
 
-  if (isFetching) {
     return (
-      <SkeletonForAll
-        withSlider={false}
-        cardCount={24}
-        sectionCount={1}
-      />
-    );
-  }
+      <div className="min-h-screen custom-page px-0 bg-[#151d25]">
+        <Helmet>
+          <title>{title}</title>
+          <meta name="description" content={metaDescription} />
+          <link
+            rel="canonical"
+            href={
+              categorySlug
+                ? `https://cuongphim.vercel.app/${categorySlug}`
+                : `https://cuongphim.vercel.app/${selectedType}/${selectedSlug}`
+            }
+          />
+        </Helmet>
 
-  return (
-    <div className='min-h-screen custom-page px-0 bg-[#151d25]'>
-      <Helmet>
-        <title>{title}</title>
-        <meta
-          name='description'
-          content={metaDescription}
-        />
-        <link
-          rel='canonical'
-          href={categorySlug ? `https://cuongphim.vercel.app/${categorySlug}` : `https://cuongphim.vercel.app/${selectedType}/${selectedSlug}`}
-        />
-      </Helmet>
-      ;
-      <NoteViewer
-        note={noteLine}
-        hidden={`hidden`}
-      />
-      <ToastContainer />
-      <Filter />
-      <div className='bg-[#151d25] border-t border-t-[#1e2732] custom-page lg:flex shadow-lg  min-h-screen'>
-        <div className='lg:mr-5 mb-5 lg:w-3/4'>
-          <div className='mb-3'>
-            <BreadCrumb
-              OthersBreadCrumb={OthersBreadCrumb}
-              categoryBreadCrumb={categoryBreadCrumb}
-              PageBreadCrumb={`Trang ${currentPage}`}
-              hiddenOther={hiddenOther}
-            />
-            <SectionTitle
-              sectionFilm={sectionTitle}
-              hidden={`hidden`}
+        <NoteViewer note={noteLine} hidden={`hidden`} />
+        <ToastContainer />
+        <Filter />
+        <div className="bg-[#151d25] border-t border-t-[#1e2732] custom-page lg:flex shadow-lg  min-h-screen">
+          <div className="lg:mr-5 mb-5 lg:w-3/4">
+            <div className="mb-3">
+              <BreadCrumb
+                OthersBreadCrumb={OthersBreadCrumb}
+                categoryBreadCrumb={categoryBreadCrumb}
+                PageBreadCrumb={`Trang ${currentPage}`}
+                hiddenOther={hiddenOther}
+              />
+              <SectionTitle sectionFilm={sectionTitle} hidden={`hidden`} />
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 responsive-edit gap-2.5">
+              {renderMovieItems}
+            </div>
+          </div>
+          <div className="lg:w-2/6">
+            <TrendingNow
+              numberSlice={numberSlice}
+              addClass={"mt-5"}
+              movieSortValue={movieSortValue}
             />
           </div>
-          <div className='grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 responsive-edit gap-2.5'>{renderMovieItems}</div>
         </div>
-        <div className='lg:w-2/6'>
-          <TrendingNow
-            numberSlice={numberSlice}
-            addClass={'mt-5'}
-            movieSortValue={movieSortValue}
+        <div className="fixed bottom-0 bg-black/75 z-10">
+          <PaginationCom
+            routePath={location.pathname}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalPages={totalPages}
+            pageType={pageType}
           />
         </div>
       </div>
-      <div className='fixed bottom-0 bg-black/75 z-10'>
-        <PaginationCom
-          routePath={location.pathname}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-          totalPages={totalPages}
-          pageType={pageType}
-        />
-      </div>
-    </div>
-  );
-});
+    );
+  },
+);
 MovieCategory.propTypes = {
   dataResults: PropTypes.array,
   categoryBreadCrumb: PropTypes.string,
@@ -165,5 +188,5 @@ MovieCategory.propTypes = {
   totalItemsSearch: PropTypes.number,
   hiddenOther: PropTypes.string,
 };
-MovieCategory.displayName = 'MovieCategory';
+MovieCategory.displayName = "MovieCategory";
 export default MovieCategory;
